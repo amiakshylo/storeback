6c6874e063d2de788f59a4889d6ba561e8e3a555
miakshylo <a.miakshylo@gmail.com>
2024-01-17 12:35:24 -0700

    adding router

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('products', views.ProductVievSet)
+router.register('collections', views.CollectionViewSet)
+router.register('customers', views.CustomerViewSet)
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),  # """aply converter to parametr 'id', int: """
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
-    path('collections/', views.CollectionList.as_view()),
-    path('customers/', views.CustomerList.as_view()),
-    path('customers/<int:pk>/', views.CustomerDetail.as_view())
-]
+urlpatterns = router.urls
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>/', views.ProductDetail.as_view()),  # """aply converter to parametr 'id', int: """
+#     path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
+#     path('collections/', views.CollectionList.as_view()),
+#     path('customers/', views.CustomerList.as_view()),
+#     path('customers/<int:pk>/', views.CustomerDetail.as_view())
+# ]
