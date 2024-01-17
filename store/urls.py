ab852018b3486eb7642040523da2e7197aba719c
miakshylo <a.miakshylo@gmail.com>
2024-01-17 11:06:10 -0700

    Implementation generic Viev

--- store/urls.py
+++ store/urls.py
@@ -4,9 +4,9 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetail.as_view()),  # """aply converter to parametr 'id', int: """
-    path('collections/<int:id>/', views.CollectionDetail.as_view(), name='collection-detail'),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),  # """aply converter to parametr 'id', int: """
+    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail'),
     path('collections/', views.CollectionList.as_view()),
     path('customers/', views.CustomerList.as_view()),
-    path('customers/<int:id>/', views.CustomerDetail.as_view())
+    path('customers/<int:pk>/', views.CustomerDetail.as_view())
 ]
