5e9ed4b9ae70664439dfc64101cec97fb611b7af
miakshylo <a.miakshylo@gmail.com>
2024-01-17 17:34:38 -0700

    clean up the code

--- store/views.py
+++ store/views.py
@@ -1,8 +1,6 @@
 from typing import Collection
-from django.shortcuts import get_object_or_404
+
 from django.db.models.aggregates import Max, Min, Avg, Sum, Count
-from django.db.models.functions import Concat
-from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django_filters.rest_framework import DjangoFilterBackend
 from store.filters import ProductFilter
 from .models import Customer, Order, OrderItem, Product, Collection, Review
@@ -10,15 +8,9 @@ from .serializers import CollectionSerializer, CustomerSerializer, ProductSerial
 """insted of using built-in django classes HttpResponse and HttpResponse
 we should use resr_framefork classes
 """
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
-
-"""implementation with mixin and ListCreateAPIView"""
 
 
 class ProductVievSet(ModelViewSet):  # or ReadOnlyModelViewSet, only for GETing
@@ -34,43 +26,6 @@ class ProductVievSet(ModelViewSet):  # or ReadOnlyModelViewSet, only for GETing
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': 'Product can not be deleted because it is associated with an order item '}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
-
-    """If we wanna have some logic for creating queryset or serializer
-    for example some condition"""
-    # def get_queryset(self):
-    #     return Product.objects.select_related('collection').all()
-
-    # def get_serializer_class(self):
-    #     return ProductSerializer
-
-
-"""implementation with APIView class"""
-# class ProductList(APIView):
-#     def get(self, request):
-#         queryset = Product.objects.select_related('collection').all()
-#         serializer = ProductSerializer(
-#             queryset, many=True, context={'request': request})
-#         return Response(serializer.data)
-
-#     def post(self, request):
-#         serializer = ProductSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-"""Long method"""
-# @api_view(['GET', 'POST'])
-# def product_list(request):
-# if request.method == 'GET':
-#     queryset = Product.objects.select_related('collection').all()
-#     serializer = ProductSerializer(queryset, many=True, context={'request': request})
-#     return Response(serializer.data)
-# elif request.method == 'POST':
-#     serializer = ProductSerializer(data=request.data)
-#     serializer.is_valid(raise_exception=True)
-#     serializer.save()
-#     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class CollectionViewSet(ModelViewSet):
