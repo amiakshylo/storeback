37b7d651d7e028103785070bf7e680ff6899f6d8
miakshylo <a.miakshylo@gmail.com>
2024-01-19 10:39:53 -0700

    bug fixing

--- store/views.py
+++ store/views.py
@@ -16,7 +16,7 @@ class ProductVievSet(ModelViewSet):  # or ReadOnlyModelViewSet, only for GETing
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
-    pagination_class = DefaultPagination()
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
