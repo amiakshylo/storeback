3e47f20c37e16a86dd92a79fd70261a370b7881f
miakshylo <a.miakshylo@gmail.com>
2024-01-25 11:38:21 -0700

    changing ACCESS_TOKEN_TIMELINE

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -166,6 +167,10 @@ DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer'
     }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
 
 
