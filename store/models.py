b04de1a58c31417abcbc6d1c2faf1eedd9dc0030
miakshylo <a.miakshylo@gmail.com>
2024-02-09 15:41:58 -0700

    validate image size with custom validators

--- store/models.py
+++ store/models.py
@@ -1,9 +1,9 @@
-from itertools import product
 from django.db import models
 from django.contrib import admin
 from django.core.validators import MinValueValidator
 from uuid import uuid4
 from django.conf import settings
+from .validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -50,7 +50,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images', validators=[validate_file_size])
 
 
 class Review(models.Model):
