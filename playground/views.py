387ea8409ddb58a664fc50c06073a0ba64aab07e
miakshylo <a.miakshylo@gmail.com>
2024-02-13 10:41:41 -0700

    EmailMessage class (attach image for instance)

--- playground/views.py
+++ playground/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from django.core.mail import mail_admins, send_mail, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 
 
 def say_hello(request):
     try:
-        send_mail('subject', 'message', 'user@gmail.com', ['bob@gmail.com'], html_message='message')
+        message = EmailMessage('subject', 'message', 'user@gmail.com', ['bob@gmail.com'])
+        message.attach_file('playground/static/images/dog.jpeg')
+        message.send()
     except BadHeaderError:
         pass
 
-    try:
-        mail_admins('subject', 'message')
-    except BadHeaderError:
-        pass
+    # try:
+    #     mail_admins('subject', 'message')
+    # except BadHeaderError:
+    #     pass
         
     
     return render(request, 'hello.html', {'name': 'Andrii'})
