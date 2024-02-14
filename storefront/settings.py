e0d608c44d6f9e975e17c83813a228d5d8e3bbab
Andrew Ocean <a.miakshylo@gmail.com>
2024-02-14 11:52:02 -0700

    scheduling periodic tasks

--- storefront/settings.py
+++ storefront/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -199,4 +200,12 @@ DJOSER = {
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers' : {
+        'task': 'playground.tasks.notify_customers',
+        'schedule': 5,
+        # 'schedule': crontab(),
+        'args': ['Hello World']
+    }
+}
 
