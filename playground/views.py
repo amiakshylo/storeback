b17962c2f9b1709402478b578f1740ff987c737b
Andrew Ocean <a.miakshylo@gmail.com>
2024-02-14 11:35:06 -0700

    creating and executing tasks

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from django.core.mail import BadHeaderError, EmailMessage
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
+
 
 def say_hello(request):
-    try:
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Andrew'}
-        )
-        message.send(['bob@gmail.com'])
-    except BadHeaderError:
-        pass
-            
+    notify_customers.delay('Hello')        
     return render(request, 'hello.html', {'name': 'Andrii'})
