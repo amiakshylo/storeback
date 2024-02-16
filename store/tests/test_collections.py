b961893e90d1a1d483079fff4ac2c979c3301baa
Andrew Ocean <a.miakshylo@gmail.com>
2024-02-15 18:02:10 -0700

    bug fixing

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -7,12 +7,12 @@ import pytest
 class TestCreateCollection:
     def test_if_user_is_anonynous_return_401(self):
         client = APIClient()
-        response = client.post('/store/collections/', {})
+        response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED #type: ignore
         
     def test_if_user_is_not_admin_return_403(self):
         client = APIClient()
-        client.force_authenticate(user={'login': 'admin', 'password': ''})
+        client.force_authenticate(user={})
         response = client.post('/store/collections/', {'title': 'a'})
         assert response.status_code == status.HTTP_403_FORBIDDEN #type: ignore
         
