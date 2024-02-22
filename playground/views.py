69da7d8e9f1190c7fb16d52c884b507e9354ab99
Andrew Ocean <a.miakshylo@gmail.com>
2024-02-22 12:21:31 -0700

    cashing for function based views

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 import requests
+from django.views.decorators.cache import cache_page
 
 
-
+@cache_page(10 * 60)
 def say_hello(request):
-    requests.get('https://httpbin.org/delay/2')
-    return render(request, 'hello.html', {'name': 'Andrii'})
+    response = requests.get('https://httpbin.org/delay/2')
+    data = response.json()
+    return render(request, 'hello.html', {'name': data})
