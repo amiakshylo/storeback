a99999e8f6691a915be0a4b0b2d2aa85d62ad47f
Andrew Ocean <a.miakshylo@gmail.com>
2024-02-22 13:49:48 -0700

    collecting static assets before deployment

--- storefront/settings.py
+++ storefront/settings.py
@@ -166,6 +166,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
