f1711cb7c0d39f18b3ace737ef357dea7e4e5c76
Andrew Ocean <a.miakshylo@gmail.com>
2024-02-23 15:11:51 -0700

    //

--- store/models.py
+++ store/models.py
@@ -131,7 +131,8 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+
+    uuid = models.UUIDField(primary_key=True,default=uuid4, unique=True)
     
     
     created_at = models.DateTimeField(auto_now_add=True)
