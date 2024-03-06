15540139fb4a006ae2ddb738ac5b7504024d25e3
Andrew Ocean <a.miakshylo@gmail.com>
2024-03-05 17:15:49 -0700

    //

--- build.sh
+++ build.sh
@@ -10,10 +10,7 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-# Create superuser
-#python manage.py createsuperuser_if_not_exists
-
-#Populate DB
-#python manage.py seed_db
+# Populate DB
+python manage.py seed_db
 
 
