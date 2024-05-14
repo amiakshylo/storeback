bef027f2f442c3b0c51cb32edd9b210bf8b273cf
AndrewM <a.miakshylo@gmail.com>
2024-05-14 11:33:13 -0600

    deploying

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -99,7 +99,7 @@ USE_L10N = True
 USE_TZ = False
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
